047685c1874169ed70b8e9c647a1793ad3c0a1d7
luisa.colorado <luisa.colorado.e@gmail.com>
2023-03-09 17:18:41 -0500

    LLOGER info variable added

--- src/test/java/co/com/training/web/tests/MyCoolListener.java
+++ src/test/java/co/com/training/web/tests/MyCoolListener.java
@@ -1,11 +1,14 @@
 package co.com.training.web.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class MyCoolListener implements ITestListener {
 
+    private static final Logger LOGGER = LogManager.getLogger(MyCoolListener.class.getName());
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
@@ -14,12 +17,12 @@ public class MyCoolListener implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        System.out.println("Test pasó ! " + iTestResult.getName());
+        LOGGER.info("Test case passed: {}", iTestResult.getName());
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.println("Test falló :( " + iTestResult.getName());
+        LOGGER.info("Test case failed: {}" ,iTestResult.getName());
     }
 
     @Override
