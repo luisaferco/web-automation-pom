06b4279fa3ade8a20359f7eb243e7dce87c5b588
luisa.colorado <luisa.colorado.e@gmail.com>
2023-06-24 19:48:15 -0500

    Env config class has been created to configure capabilities and server connection to browserstack and local envs

---
+++ src/main/java/co/com/training/web/config/env/EnvironmentConfig.java
@@ -0,0 +1,75 @@
+package co.com.training.web.config.env;
+
+import co.com.training.web.utils.maps.Adapter;
+import com.browserstack.local.Local;
+import org.json.simple.JSONObject;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnvironmentConfig {
+
+
+    private JSONObject config;
+    private ServerAddress serverAddress;
+    private Map<String, Object> commonCapabilities;
+
+    public EnvironmentConfig(JSONObject config) {
+        this.config = config;
+
+    }
+
+    public static EnvironmentConfig with(JSONObject config) {
+        return new EnvironmentConfig(config);
+    }
+
+    public void createServerConnection() {
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        String password = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        String server =  config.get("server").toString();
+        username = username == null? config.get("user").toString() : username;
+        password = password == null? config.get("key").toString() : password;
+        this.serverAddress = new ServerAddress(username,password,server);
+    }
+
+    public void setUp(){
+        createServerConnection();
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
+        HashMap bstackOptionsMap = (HashMap) commonCapabilities.get("bstack:options");
+        if(bstackOptionsMap.get("local") != null &&
+            bstackOptionsMap.get("local").toString().equalsIgnoreCase("true")) {
+            Local local = new Local();
+            try {
+                local.start(Collections.singletonMap("key", serverAddress.getPassword()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public DesiredCapabilities setUp(String environment) {
+        JSONObject envs = (JSONObject) config.get("environments");
+        Map<String, Object> envCapabilities = (Map<String, Object>) envs.get(environment.toLowerCase());
+
+        Adapter.mapJsonToMap(envs, envCapabilities,"");
+
+        DesiredCapabilities commonCapabilities = new DesiredCapabilities();
+        for (Map.Entry<String, Object> cap : envCapabilities.entrySet()) {
+            commonCapabilities.setCapability(cap.getKey(),cap.getValue());
+        }
+        JSONObject caps = (JSONObject) config.get("capabilities");
+        Map<String, Object> capabilities = (Map<String, Object>) caps.get("bstack:options");
+
+        Adapter.mapJsonToMap(envs, capabilities,"");
+        for (Map.Entry<String, Object> cap : capabilities.entrySet()) {
+            commonCapabilities.setCapability(cap.getKey(),cap.getValue());
+        }
+        return commonCapabilities;
+    }
+
+
+    public ServerAddress getServerAddress() {
+        return serverAddress;
+    }
+}
