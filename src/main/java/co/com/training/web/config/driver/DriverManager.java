f84f9a3b02e0bdf9b0050717c6cd7867a8bbecec
luisa.colorado <luisa.colorado.e@gmail.com>
2023-06-24 19:50:17 -0500

    Drivermanager class has been refactored to add capabilities and address by environment browser

--- src/main/java/co/com/training/web/config/driver/DriverManager.java
+++ src/main/java/co/com/training/web/config/driver/DriverManager.java
@@ -1,20 +1,42 @@
 package co.com.training.web.config.driver;
 
+import co.com.training.web.config.env.ServerAddress;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class DriverManager extends ConfigCapabilities {
 
     private WebDriver webDriver;
+    private String browserName;
+    private DesiredCapabilities capabilities;
+    private ServerAddress address;
 
-    private DriverManager(String browserName) {
+    private DriverManager(String browserName, ServerAddress address) {
         super();
-        DesiredCapabilities desiredCapabilities = super.loadCapabilities();
-        this.webDriver = BrowserType.valueOf(browserName).createDriver(desiredCapabilities);
+        this.browserName = browserName;
+        this.address = address;
+        this.capabilities = super.loadCapabilities();
+        this.webDriver = BrowserType.valueOf(browserName).createDriver(capabilities, address);
+
     }
 
-    public static DriverManager newDriver(String browserName){
-        return new DriverManager(browserName);
+    private DriverManager(String browserName, DesiredCapabilities capabilities,ServerAddress address) {
+        super();
+        this.browserName = browserName;
+        this.address = address;
+        this.capabilities = super.loadCapabilities().merge(capabilities);
+        this.webDriver = BrowserType.valueOf(browserName).createDriver(capabilities, address);
+
+    }
+
+    public static DriverManager newDriver(String browserName, ServerAddress serverAddress){
+        return new DriverManager(browserName, serverAddress);
+    }
+
+    public static DriverManager newDriver(String browserName,
+                                          DesiredCapabilities capabilities,
+                                          ServerAddress serverAddress){
+        return new DriverManager(browserName, capabilities, serverAddress);
     }
 
     public WebDriver getWebDriver(String url) {
