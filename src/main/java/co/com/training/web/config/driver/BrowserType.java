480fc3e2e3d69d2ab1782b62248b715efd61d6c3
luisa.colorado <luisa.colorado.e@gmail.com>
2023-06-24 19:51:29 -0500

    new parameter added in createDriver method.

--- src/main/java/co/com/training/web/config/driver/BrowserType.java
+++ src/main/java/co/com/training/web/config/driver/BrowserType.java
@@ -1,46 +1,66 @@
 package co.com.training.web.config.driver;
 
+import co.com.training.web.config.env.ServerAddress;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.http.ClientConfig;
+
+import java.time.Duration;
 
 public enum BrowserType {
 
     CHROME {
         @Override
-        public WebDriver createDriver(Capabilities capabilities) {
+        public WebDriver createDriver(Capabilities capabilities, ServerAddress serverAddress) {
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.addArguments("--headless");
             chromeOptions.merge(capabilities);
             WebDriverManager.chromedriver().setup();
-            return new ChromeDriver(chromeOptions);
+            ClientConfig customConfig = ClientConfig.defaultConfig().readTimeout(Duration.ofMinutes(15))
+                    .connectionTimeout(Duration.ofMinutes(15));
+            return RemoteWebDriver.builder()
+                    .address(serverAddress.getUrlConnection())
+                    .config(customConfig)
+                    .oneOf(chromeOptions)
+                    .build();
 
         }
     },
     EDGE {
         @Override
-        public WebDriver createDriver(Capabilities capabilities) {
+        public WebDriver createDriver(Capabilities capabilities, ServerAddress serverAddress) {
             EdgeOptions options = new EdgeOptions();
             options.merge(capabilities);
             WebDriverManager.edgedriver().setup();
-            return new EdgeDriver(options);
+            ClientConfig customConfig = ClientConfig.defaultConfig().readTimeout(Duration.ofMinutes(15))
+                    .connectionTimeout(Duration.ofMinutes(15));
+            return RemoteWebDriver.builder()
+                    .address(serverAddress.getUrlConnection())
+                    .config(customConfig)
+                    .oneOf(options)
+                    .build();
         }
     },
     REMOTE {
         @Override
-        public WebDriver createDriver(Capabilities capabilities) {
-
-            return new RemoteWebDriver(capabilities);
+        public WebDriver createDriver(Capabilities capabilities,ServerAddress serverAddress) {
+            ClientConfig customConfig = ClientConfig.defaultConfig().readTimeout(Duration.ofMinutes(15))
+                    .connectionTimeout(Duration.ofMinutes(15));
+            return RemoteWebDriver.builder()
+                    .address(serverAddress.getUrlConnection())
+                    .config(customConfig)
+                    .oneOf(capabilities)
+                    .build();
         }
 
     };
 
 
 
-    public abstract WebDriver createDriver(Capabilities capabilities);
+    public abstract WebDriver createDriver(Capabilities capabilities, ServerAddress serverAddress);
 }
