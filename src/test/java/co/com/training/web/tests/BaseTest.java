75bcb54e10189af99283be7dcf5687b9ef263ac9
luisa.colorado <luisa.colorado.e@gmail.com>
2023-06-24 19:49:02 -0500

    custom condition moved to another package

--- src/test/java/co/com/training/web/tests/BaseTest.java
+++ src/test/java/co/com/training/web/tests/BaseTest.java
@@ -1,21 +1,40 @@
 package co.com.training.web.tests;
 
 import co.com.training.web.config.driver.DriverManager;
+import co.com.training.web.config.env.EnvironmentConfig;
+import co.com.training.web.config.env.ReadConfigFile;
+import co.com.training.web.config.env.ServerAddress;
 import co.com.training.web.pageobject.NavigationPage;
+import org.json.simple.JSONObject;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 
 public abstract class BaseTest {
 
+    private static JSONObject configValues;
     private static DriverManager driverManager;
+    private static EnvironmentConfig envConfig;
     public NavigationPage navigationPage;
+
+    @BeforeSuite(alwaysRun = true)
+    @Parameters({"conf"})
+    public void beforeSuite(String config){
+        ReadConfigFile configFile = ReadConfigFile.getInstance(config);
+        configValues = configFile.getConfig();
+        envConfig = EnvironmentConfig.with(configValues);
+        envConfig.setUp();
+    }
 
     @BeforeMethod(alwaysRun = true)
     @Parameters({"browser","url"})
     public void beforeMethod(String browser, String url) {
-        driverManager = DriverManager.newDriver(browser);
+        DesiredCapabilities capabilities = envConfig.setUp(browser);
+        ServerAddress address = envConfig.getServerAddress();
+        driverManager = DriverManager.newDriver(browser, capabilities, address);
         navigationPage = new NavigationPage(driverManager.getWebDriver(url));
     }
 
